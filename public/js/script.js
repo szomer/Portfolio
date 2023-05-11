9fc58af4c773b4caa46ad6b0259fd3ab4101e69e
Suzanne Zomer <suzannezomer@gmail.com>
2023-05-11 16:41:52 +0200

    Minor improvements to prepare finilazation of website

--- public/js/script.js
+++ public/js/script.js
@@ -1,16 +1,22 @@
-//--------
-// hanlde forest image size
+// Transition Screen-------------------------------------------------
+const transition = document.querySelector('#transition');
+setTimeout(() => {
+  transition.classList.add('hidden');
+}, 2500);
+
+// Apple hide navbars---------------------------------------------------
+window.scrollTo(0, 1);
+
+// Handle Forest Banner-------------------------------------------------
 const popoverTriggerList = document.querySelectorAll(
   '[data-bs-toggle="popover"]'
 );
 const popoverList = [...popoverTriggerList].map(
   (popoverTriggerEl) => new bootstrap.Popover(popoverTriggerEl)
 );
-
 $(window).resize(function () {
   setHomeForestWidth();
 });
-
 function setHomeForestWidth() {
   var div = $('.home-forest');
   var width = div.width();
@@ -22,22 +28,18 @@ function setHomeForestWidth() {
   }
   div.css('height', height);
 }
-
 setHomeForestWidth();
 
-//---------
-// theme
+// Theme ---------------------------------------------------------
 const themeCheckBox = document.querySelector('#themebtn');
 const bodyElement = document.querySelector('body');
 const videoClouds = document.querySelector('.video-clouds');
 const videoMoon = document.querySelector('.video-moon');
 const loader = document.querySelector('#transition');
 const loaderText = document.querySelector('#transition-text');
-
 themeCheckBox.addEventListener('change', () => {
   // loading animation
   load();
-
   // set theme
   if (themeCheckBox.checked) {
     bodyElement.classList.add('theme-light');
@@ -49,7 +51,6 @@ themeCheckBox.addEventListener('change', () => {
     videoMoon.classList.add('hidden');
   }
 });
-
 // function for loading screen
 function load() {
   loaderText.classList.remove('hidden');
@@ -60,4 +61,17 @@ function load() {
     loaderText.classList.add('hidden');
     loader.classList.remove('animate-loader');
   }, 2000);
+}
+
+// Projects Slider-------------------------------------------------
+const cards = document.getElementsByClassName('card');
+for (let card of cards) {
+  card.addEventListener('click', () => {
+    if (!card.classList.contains('active')) {
+      for (let c of cards) {
+        c.classList.remove('active');
+        card.classList.add('active');
+      }
+    }
+  });
 }
--- public/js/transition.js
+++
@@ -1,5 +0,0 @@
-const transition = document.querySelector('#transition');
-
-setTimeout(() => {
-  transition.classList.add('hidden');
-}, 2200);
